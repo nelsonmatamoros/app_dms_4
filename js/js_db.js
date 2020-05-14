42fd2ffb763a4ddeb83a2a64b59329f0448ecb38
nelsonmatamoros <44848911+nelsonmatamoros@users.noreply.github.com>
2020-05-13 19:57:21 -0600

    Add files via upload

--- js/js_db.js
+++ js/js_db.js
@@ -1,8 +1,9 @@
 
 
-var db = openDatabase('db_dms_forms', '1.0', 'DB for DMS App Forms', 20 * 1024 * 1024);
+var db = openDatabase('db_dms_forms', '1.0', 'DB for DMS App Forms', 30 * 1024 * 1024);
 
 db.transaction(function(cmd){
+    
     var vFlag = 0;
     cmd.executeSql('CREATE TABLE IF NOT EXISTS users (id unique, pwd, name, phone, email, job_title, status, login, type, id_dms, license, id_pdv_dlr)');
     cmd.executeSql('CREATE TABLE IF NOT EXISTS params (id unique, dvalue)');
@@ -11,6 +12,8 @@ db.transaction(function(cmd){
     cmd.executeSql('CREATE TABLE IF NOT EXISTS tbl_forms_filled (id_form, dtos, date, status, lat, lng)'); 
     cmd.executeSql('CREATE TABLE IF NOT EXISTS tbl_files (id_file, correl, name, type, strdtos)'); 
     cmd.executeSql('CREATE TABLE IF NOT EXISTS tbl_ventas(particion, anomes, id_dms, producto, unidad, meta, monto)');
+    cmd.executeSql('CREATE TABLE IF NOT EXISTS tbl_nodos(nodo, coordenadas)');
+    cmd.executeSql('CREATE TABLE IF NOT EXISTS tbl_horus_marcas_esp(LATITUD,LONGITUD,ID_PDV,FECHA_COMPLETA,USUARIO,NUMERO_CLIENTE,ESTADO_PAGO,METODO_PAGO,MONTO_PAGO )');
     cmd.executeSql('CREATE TABLE IF NOT EXISTS tbl_plan_dms(aniomes, semana_anio, usuario, cod_empleado_dms, circuit, nombre_circuito, id_pdv, nombre_pdv, dias_semana, ymd_dia, monto_credito)');    
     cmd.executeSql('CREATE TABLE IF NOT EXISTS tbl_ficha_pdv(id_pdv unique, nombre_pdv, duenio, dir, mbl_epin, mbl_tmy, segmento_pop)');
     cmd.executeSql('CREATE TABLE IF NOT EXISTS tbl_visitas_pdv(id_pdv, fecha_ymd, usuario)');
@@ -41,6 +44,7 @@ function ejecutaSQL(vQuery, vFlag){
             },function(e){
                 //alert('e');
                 console.log('Error' + e.error);
+                console.log(e);
                 //window.plugins.toast.show('Error..', 1000, 'bottom');
             });
     });
